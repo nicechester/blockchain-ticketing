a206b98f81b12ebcc90c418b503a21331dce3675
nicechester <violkim@gmail.com>
2025-08-22 16:19:31 -0700

    Account management

--- main/java/com/nicechester/blockchain/ticketing/Account.java
+++ main/java/com/nicechester/blockchain/ticketing/Account.java
@@ -1,28 +1,11 @@
 package com.nicechester.blockchain.ticketing;
 
+import lombok.Value;
+
+@Value
 public class Account {
-    private String address;
-    private String name;
-
-    public Account(String address, String name) {
-        this.address = address;
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    private final String address;
+    private final String privateKey;
+    private final String name;
 }
 
--- main/java/com/nicechester/blockchain/ticketing/AccountService.java
+++ main/java/com/nicechester/blockchain/ticketing/AccountService.java
@@ -1,30 +1,52 @@
 package com.nicechester.blockchain.ticketing;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Log4j2
 @Component
 public class AccountService {
-    @Autowired Wallet wallet;
+    @Autowired private ObjectMapper objectMapper;
 
+    @Setter
     private List<Account> accounts;
-    private static final String[] FULL_NAMES = {
-        "Alice Marie Smith", "Bob Johnson", "Charlie Lee Williams", "Diana Ann Brown", "Edward James Jones", "Fiona Lynn Garcia", "George Paul Miller", "Hannah Grace Davis", "Ivan Ray Martinez", "Julia Kim Clark", "Kevin Allen Lewis", "Laura Jean Walker", "Michael Scott Young", "Nina Sue Allen", "Oscar Max King", "Paula Lou Wright", "Quentin Dean Scott", "Rachel Rae Green", "Sam Jude Baker", "Tina Jo Adams"
-    };
 
     @PostConstruct
     public void init() {
-        accounts = new ArrayList<>();
-        int i = 0;
-        for (String address : wallet.getKeystore().keySet()) {
-            String name = FULL_NAMES[i % FULL_NAMES.length];
-            accounts.add(new Account(address, name));
-            i++;
+        accounts = loadAccounts();
+    }
+
+    private void saveAccounts(List<Account> accounts) {
+        File jsonFile = new File("accounts.json");
+        try {
+            objectMapper.writeValue(jsonFile, accounts);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        log.info("{} accounts were saved to file: {}", accounts.size(), jsonFile.getAbsolutePath());
+    }
+
+    private List<Account> loadAccounts() {
+        File jsonFile = new File("accounts.json");
+        List<Account> accountsFromFile;
+        try {
+            accountsFromFile = objectMapper.readValue(jsonFile, new TypeReference<List<Account>>() {});
+        } catch (IOException e) {
+            log.error("Loading accounts.json has been failed. begin with empty accounts", e);
+            return new ArrayList<>();
+        }
+        log.info("{} accounts were loaded to file: {}", accountsFromFile.size(), jsonFile.getAbsolutePath());
+        return accountsFromFile;
     }
 
     public List<Account> getAccounts() {
@@ -38,5 +60,13 @@ public class AccountService {
             }
         }
         return null;
+    }
+
+    public Account createAccount(String name, String address, String privateKey) {
+        Account newAccount = new Account(address, privateKey, name);
+        accounts.add(newAccount);
+        saveAccounts(accounts);
+        return newAccount;
+
     }
 }
